6c656c8f8aed1f8d7c85c4e343958a05b5b47dbd
github.nouser@nouser.github.com <github.nouser@nouser.github.com>
2023-06-08 13:40:24 +0200

    Update 2020_20002_generale.html, 2020_20002_grad_10_M.html, 2020_20002_grad_11_M.html, and 2324 more files

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react-swc"
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: { outDir: "./docs/beta" }
+  build: { outDir: "./dist" }
 })
