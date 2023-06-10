17a1c451328b65569fe670dea09a8713954aef6c
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2023-06-10 16:05:02 +0200

    fix(ci): correctly use PUBLIC_URL basename (#17)
    * fix: remove previous added router baseName
    
    * fix(ci): use preview workflow on each branch
    
    * fix: add env.PUBLIC_URL basename to react-router

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,9 @@ const routes = [
   }
 ]
 
-const router = createBrowserRouter(routes, { basename: "/RankingsBeta" })
+const router = createBrowserRouter(routes, {
+  basename: import.meta.env.BASE_URL
+})
 
 function Layout() {
   return (
