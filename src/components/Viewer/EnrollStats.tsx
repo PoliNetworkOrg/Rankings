3863d7f54339fc6aa4051ad726d76e41db14beff
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2023-06-24 13:44:38 +0200

    fix: refactor convert-to-csv function

--- src/components/Viewer/EnrollStats.tsx
+++ src/components/Viewer/EnrollStats.tsx
@@ -6,7 +6,7 @@ interface SingleProps extends React.HTMLAttributes<HTMLParagraphElement> {
 }
 const Single = ({ label, value, ...props }: SingleProps) => (
   <p
-    className="flex-1 rounded bg-slate-100 py-2 px-1 text-center dark:bg-slate-800"
+    className="flex-1 rounded bg-slate-100 px-1 py-2 text-center dark:bg-slate-800"
     {...props}
   >
     <span className="mr-2 text-black/50 dark:text-white/50">{label}</span>
