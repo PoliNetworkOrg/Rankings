b6f3dcc3e3570fe17906107a08f3173b0b04f432
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2023-06-30 15:13:30 +0200

    feat: start writing Data class and create its context

---
+++ src/contexts/DataContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useEffect, useState } from "react"
+import { Data } from "../utils/data/data"
+
+export interface IDataContext {
+  data: Data
+  isLoading: boolean
+}
+
+const DataContext = createContext<IDataContext>({
+  data: new Data(),
+  isLoading: true
+})
+
+type Props = React.HTMLAttributes<React.ProviderProps<IDataContext>>
+export function DataProvider({ ...p }: Props) {
+  const [data, setData] = useState(new Data())
+  const [isLoading, setIsLoading] = useState(true)
+
+  async function init() {
+    const data = await Data.init()
+    if (data) setData(data)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    init()
+  })
+
+  return <DataContext.Provider value={{ data, isLoading }} {...p} />
+}
+
+export default DataContext
