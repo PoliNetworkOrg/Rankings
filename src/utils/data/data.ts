b1298c680656cec7bfc283036463364d82354dd0
Lorenzo <lorenzo.corallo@outlook.it>
2023-07-03 17:54:23 +0200

    feat: add phase list getter from ranking and course

--- src/utils/data/data.ts
+++ src/utils/data/data.ts
@@ -1,8 +1,10 @@
 import urlJoin from "url-join"
 import { LINKS } from "../constants"
-import { IndexBySchool } from "../types/data/Index/BySchool"
 import School from "../types/data/School"
 import Ranking from "../types/data/Ranking"
+import { IndexBySchool } from "../types/data/Index/BySchool"
+import { IndexBySchoolYearCourse } from "../types/data/Index/BySchoolYearCourse"
+import CourseTable from "../types/data/Ranking/CourseTable"
 
 export class Data {
   protected static readonly _u = LINKS.dataBasePath
@@ -10,14 +12,22 @@ export class Data {
     this._u,
     "bySchoolYear.json"
   )
+  protected static readonly coursePhasesUrl: string = urlJoin(
+    this._u,
+    "bySchoolYear.json"
+  )
 
   public index?: IndexBySchool
+  public coursesPhases?: IndexBySchoolYearCourse
   public schools: School[] = []
   public urls: string[] = []
 
   public static async init() {
     const data = new Data()
     data.index = await fetch(Data.indexUrl).then(res => res.json())
+    data.coursesPhases = await fetch(Data.coursePhasesUrl).then(res =>
+      res.json()
+    )
     if (!data.index) return null
 
     for (const [school, years] of Object.entries(data.index.schools)) {
@@ -41,5 +51,19 @@ export class Data {
     const url = urlJoin(Data._u, school, year.toString(), filename)
     const json = await fetch(url).then(res => res.json())
     return json as Ranking
+  }
+
+  public getCoursePhases(ranking: Ranking, course: CourseTable): string[] {
+    const { title, location } = course
+    const files =
+      this.coursesPhases?.schools[ranking.school][ranking.year][title][
+        location
+      ] ?? []
+
+    const phases = files.map(file =>
+      file.link.endsWith(".json") ? file.link.split(".json")[0] : file.link
+    )
+
+    return phases
   }
 }
