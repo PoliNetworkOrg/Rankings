23e356fac2cae5e1893a2497d2de602984c26be0
Lorenzo <lorenzo.corallo@outlook.it>
2023-07-11 00:32:23 +0200

    style: transform privacy page style to tailwindcss

--- src/routes/privacy/index.tsx
+++ src/routes/privacy/index.tsx
@@ -8,43 +8,34 @@ export const privacyRoute = new Route({
   component: function Privacy() {
     return (
       <Page>
-        <div style={{ padding: 20 }}>
-          <div style={{ textAlign: "justify" }}>
-            <div style={{ padding: 10 }}></div>
-            <div style={{ textAlign: "center" }}>
-              <h1>
-                <b>Privacy</b>
-              </h1>
-            </div>
-            <div style={{ padding: 20 }}></div>
-            <div>
-              <p>
-                La privacy è un valore <b>fondante</b> di{" "}
-                <a href="https://polinetwork.org">PoliNetwork</a>.
-              </p>
-              <div style={{ padding: 10 }}> </div>
-              <p>
-                Questo sito non utilizza nessun tipo di cookie, né presenta
-                forme di pubblicità o tracciamento.{" "}
-              </p>
-              <div style={{ padding: 30 }}> </div>
-              <hr style={{ opacity: 0.1 }} />
-              <div style={{ padding: 30 }}> </div>
-              <p>
-                Potete in ogni caso consultare il nostro sito e la nostra
-                raccolta di informative privacy a{" "}
-                <a href="https://polinetwork.org/learnmore/privacy">
-                  questo link
-                </a>
-              </p>
-              <div style={{ padding: 10 }}> </div>
-              <p>
-                Se avete ulteriori dubbi{" "}
-                <a href="https://polinetwork.org/learnmore/contacts/">
-                  contattateci!
-                </a>
-              </p>
-            </div>
+        <div className="flex flex-col gap-8 p-8 text-justify">
+          <h2 className="text-center text-lg font-bold">Privacy</h2>
+          <div>
+            <p>
+              La privacy è un valore <b>fondante</b> di{" "}
+              <a href="https://polinetwork.org">PoliNetwork</a>.
+            </p>
+            <p className="mt-4">
+              Questo sito non utilizza nessun tipo di cookie, né presenta forme
+              di pubblicità o tracciamento.{" "}
+            </p>
+          </div>
+          <hr className="my-4 opacity-20" />
+          <div>
+            <p>
+              Potete in ogni caso consultare il nostro sito e la nostra raccolta
+              di informative privacy a{" "}
+              <a href="https://polinetwork.org/learnmore/privacy">
+                questo link
+              </a>
+              .
+            </p>
+            <p className="mt-4">
+              Se avete ulteriori dubbi{" "}
+              <a href="https://polinetwork.org/learnmore/contacts/">
+                contattateci!
+              </a>
+            </p>
           </div>
         </div>
       </Page>
