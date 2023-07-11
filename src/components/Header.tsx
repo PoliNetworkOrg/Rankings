f091a2dad3c89e81a05aecf94ba6bcfc0295108e
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2023-07-12 00:04:26 +0200

    style: better logo ux (#119)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ import {
   MdOutlineLightMode as LightIcon,
   MdOutlineDarkMode as DarkIcon
 } from "react-icons/md"
-import { LINKS } from "@/utils/constants"
 import logo from "@/static/logo3000.webp"
 import DarkModeContext from "@/contexts/DarkModeContext"
 import { Link } from "@tanstack/router"
@@ -13,29 +12,22 @@ export default function Header() {
   const { isDarkMode, toggleDarkMode } = useContext(DarkModeContext)
   return (
     <header className="w-full border-b border-slate-800/20 dark:border-slate-300/20">
-      <div className="mx-auto flex max-w-7xl items-center justify-between p-2">
-        <div className="flex max-sm:flex-[0.25] sm:flex-1">
-          <a
-            href={LINKS.polinetworkMain}
-            rel="noreferrer noopener"
-            target="_blank"
-            className="flex items-center gap-3 rounded-md p-1 pr-4 text-black hover:bg-slate-400/10 hover:no-underline dark:text-white dark:hover:bg-slate-200/10"
-          >
-            <img src={logo} className="h-auto w-10" />
-            <span className="text-lg font-semibold max-sm:hidden">
-              PoliNetwork
-            </span>
-          </a>
-        </div>
-        <div className="flex flex-1 items-center justify-center text-center">
+      <div className="mx-auto flex max-w-7xl items-center justify-between p-4">
+        <div className="flex flex-1">
           <Link
             to="/"
-            className="text-black hover:no-underline dark:text-white"
+            className="flex items-center gap-3 rounded-md text-black hover:no-underline dark:text-white"
           >
-            <h1 className="text-2xl font-bold max-[300px]:text-xl">Rankings</h1>
+            <img src={logo} className="h-auto w-10" />
+            <span className="max-[450px]:hidden text-lg font-semibold">
+              PoliNetwork
+            </span>
+            <h1 className="text-md rounded-md bg-slate-900/10 px-2 py-1 font-bold dark:bg-slate-100/10">
+              Rankings
+            </h1>
           </Link>
         </div>
-        <div className="flex items-center justify-end max-sm:flex-[0.25] sm:flex-1">
+        <div className="flex items-center justify-end">
           <Button
             variant="ghost"
             size="icon"
