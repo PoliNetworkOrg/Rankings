06e58214b14a70b5084ef47c1a29427b36aebacc
Lorenzo <lorenzo.corallo@outlook.it>
2023-07-13 22:22:05 +0200

    style: reduce footer font-size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -34,9 +34,9 @@ function ExternalLink({
 }
 export default function Footer() {
   return (
-    <footer className="w-full border-t border-slate-800/20 py-4 dark:border-slate-300/20">
-      <div className="mx-auto flex w-full max-w-7xl items-start justify-between gap-8 px-4 max-md:flex-col-reverse max-md:gap-4">
-        <div className="flex-1">
+    <footer className="w-full border-t border-slate-800/20 py-4 text-sm dark:border-slate-300/20">
+      <div className="mx-auto px-4 w-full max-w-7xl items-start justify-between gap-8 px-4">
+        <div className="mx-auto flex w-full max-w-7xl items-start justify-between gap-8 max-md:flex-col-reverse max-md:gap-4">
           <p>
             Made by{"  "}
             <ExternalLink href={LINKS.polinetworkMain}>
@@ -44,21 +44,20 @@ export default function Footer() {
             </ExternalLink>
           </p>
 
-          <p className="mt-2 flex-1 text-slate-400">
-            L'accuratezza o l'affidabilità dei contenuti di questo sito non è
-            garantita. Consulta il sito ufficiale del{" "}
-            <ExternalLink href="https://polimi.it">
-              Politecnico di Milano
-            </ExternalLink>{" "}
-            per controllare la tua posizione in graduatoria per immatricolarti.
-          </p>
+          <nav className="flex flex-shrink-0 justify-end gap-8">
+            <Link to="/about">About</Link>
+            <ExternalLink href={LINKS.githubSource}>Source</ExternalLink>
+            <Link to="/privacy">Privacy & Cookies</Link>
+          </nav>
         </div>
-
-        <nav className="flex flex-1 justify-end gap-8">
-          <Link to="/about">About</Link>
-          <ExternalLink href={LINKS.githubSource}>Source</ExternalLink>
-          <Link to="/privacy">Privacy & Cookies</Link>
-        </nav>
+        <p className="max-w-8xl mt-2 flex-1 text-xs text-slate-400">
+          L'accuratezza o l'affidabilità dei contenuti di questo sito non è
+          garantita. <br /> Consulta il sito ufficiale del{" "}
+          <ExternalLink href="https://polimi.it">
+            Politecnico di Milano
+          </ExternalLink>{" "}
+          per controllare la tua posizione in graduatoria per immatricolarti.
+        </p>
       </div>
     </footer>
   )
