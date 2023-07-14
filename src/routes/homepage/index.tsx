fe9125bb277977245cb40b65b1fb8d8c28f82f8d
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2023-07-14 15:13:33 +0200

    style(Footer): add PoliNetwork link, disclaimer and restyle (#138)
    * style(Footer): add PoliNetwork link and re-order links
    
    * style: fix order and change PoliNetwork link title
    
    * style: new footer
    
    * style: reduce footer font-size
    
    * style: update footer

--- src/routes/homepage/index.tsx
+++ src/routes/homepage/index.tsx
@@ -1,5 +1,4 @@
 import { Outlet, Route } from "@tanstack/router"
-import Alert from "@/components/custom-ui/Alert"
 import Page from "@/components/custom-ui/Page"
 import { rootRoute } from "../root"
 import PathBreadcrumb from "@/components/PathBreadcrumb"
@@ -14,18 +13,6 @@ export const homepageRoute = new Route({
         <div className="flex w-full flex-1 flex-col items-center gap-4 py-4">
           <Outlet />
         </div>
-
-        <Alert level="warning">
-          <p>
-            L'accuratezza o l'affidabilità dei contenuti di questo sito non è
-            garantita.
-          </p>
-          <p className="italic">
-            Se vuoi controllare la tua posizione nella graduatoria per
-            immatricolarti, consulta il sito del{" "}
-            <a href="https://polimi.it">Politecnico di Milano</a>.
-          </p>
-        </Alert>
       </Page>
     )
   }
