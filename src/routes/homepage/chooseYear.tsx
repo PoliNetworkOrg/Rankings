8859c157ad61bf4d8fa44d4d55984c48bd88f337
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2023-07-15 15:24:11 +0200

    feat: add instructions in chooseYear and choosePhase (#151)

--- src/routes/homepage/chooseYear.tsx
+++ src/routes/homepage/chooseYear.tsx
@@ -30,22 +30,29 @@ export const chooseYearRoute = new Route({
     const { school } = useParams();
 
     return (
-      <ButtonGrid length={years.length}>
-        {years
-          .sort((a, b) => b - a)
-          .map((year) => (
-            <Link
-              to="/home/$school/$year"
-              params={{ school, year }}
-              key={year}
-              className="h-full"
-            >
-              <Button size="card" variant="secondary" className="h-full w-full">
-                <span className="text-lg">{year}</span>
-              </Button>
-            </Link>
-          ))}
-      </ButtonGrid>
+      <>
+        <p className="w-full text-xl">Scegli un anno di immatricolazione</p>
+        <ButtonGrid length={years.length}>
+          {years
+            .sort((a, b) => b - a)
+            .map((year) => (
+              <Link
+                to="/home/$school/$year"
+                params={{ school, year }}
+                key={year}
+                className="h-full"
+              >
+                <Button
+                  size="card"
+                  variant="secondary"
+                  className="h-full w-full"
+                >
+                  <span className="text-lg">{year}</span>
+                </Button>
+              </Link>
+            ))}
+        </ButtonGrid>
+      </>
     );
   },
 });
