bc9f7adb2baa2483d7051ae3ca1078a215294107
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2023-07-30 12:28:21 +0200

    feat: change course select behaviour (#168)
    * feat: create custom-ui component Removable
    
    * feat: change course select behaviour

---
+++ src/components/custom-ui/Removable.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/utils/ui.ts";
+import { IoCloseCircle } from "react-icons/io5";
+
+const removableVariants = cva(
+  "inline-flex items-center justify-center rounded-md text-sm font-medium disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-slate-800",
+  {
+    variants: {
+      variant: {
+        default: "bg-slate-100 text-black dark:bg-slate-800 dark:text-white",
+      },
+      size: {
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-md px-3 text-xs",
+        lg: "h-10 rounded-md px-8",
+        card: "h-full p-8",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+);
+
+export interface RemovableProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof removableVariants> {
+  asChild?: boolean;
+  onRemove?: () => void;
+  showRemove?: boolean;
+}
+
+const Removable = React.forwardRef<HTMLDivElement, RemovableProps>(
+  (
+    {
+      className,
+      variant,
+      size,
+      showRemove = true,
+      asChild = false,
+      children,
+      onRemove,
+      ...props
+    },
+    ref,
+  ) => {
+    const Comp = asChild ? Slot : "div";
+    return (
+      <Comp
+        className={cn(removableVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      >
+        {children}
+        {showRemove && (
+          <button onClick={onRemove} className="ml-2">
+            <IoCloseCircle size={26} />
+          </button>
+        )}
+      </Comp>
+    );
+  },
+);
+Removable.displayName = "Removable";
+
+export { Removable, removableVariants };
--- src/routes/viewer/CourseCombobox.tsx
+++ src/routes/viewer/CourseCombobox.tsx
@@ -1,3 +1,4 @@
+import { Removable } from "@/components/custom-ui/Removable";
 import { Button } from "@/components/ui/button";
 import {
   Command,
@@ -23,45 +24,55 @@ type Props = {
   onSelect: (value: string) => void;
 };
 
-export function CourseCombobox({ value, courses, onSelect }: Props) {
+export function CourseCombobox({ value, courses: c, onSelect }: Props) {
   const [open, setOpen] = useState(false);
+  function handleSelect(value: string) {
+    onSelect(value);
+    setOpen(false);
+  }
+
+  const arr = c.valuesArr();
+  const absCourse = arr[0];
+  const courses = arr.slice(1);
+  const selectedCourse = courses.find((a) => a.value === value);
 
   return (
     <div className="flex items-center space-x-4">
       <p className="text-muted-foreground text-sm">Corso</p>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button variant="outline" className="h-full justify-start">
-            {value ? courses.get(value)?.label : "Seleziona un corso..."}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="p-0" side="bottom" align="start">
-          <Command>
-            <CommandInput placeholder="Cambia corso..." />
-            <CommandList>
-              <CommandEmpty>No courses found.</CommandEmpty>
-              <CommandGroup value={value}>
-                <ScrollArea
-                  className={courses.valuesArr().length > 7 ? "h-72" : ""}
-                >
-                  {courses.valuesArr().map((course) => (
-                    <CommandItem
-                      key={course.value}
-                      value={course.value}
-                      onSelect={(value) => {
-                        onSelect(value);
-                        setOpen(false);
-                      }}
-                    >
-                      {course.label}
-                    </CommandItem>
-                  ))}
-                </ScrollArea>
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
+      {selectedCourse ? (
+        <Removable onRemove={() => handleSelect(absCourse.value)}>
+          {selectedCourse.label}
+        </Removable>
+      ) : (
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button variant="outline" className="h-full justify-start">
+              Nessun corso selezionato
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="p-0" side="bottom" align="start">
+            <Command>
+              <CommandInput placeholder="Cambia corso..." />
+              <CommandList>
+                <CommandEmpty>Nessun corso trovato</CommandEmpty>
+                <CommandGroup>
+                  <ScrollArea className={courses.length > 7 ? "h-72" : ""}>
+                    {courses.map((course) => (
+                      <CommandItem
+                        key={course.value}
+                        value={course.value}
+                        onSelect={handleSelect}
+                      >
+                        {course.label}
+                      </CommandItem>
+                    ))}
+                  </ScrollArea>
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
+      )}
     </div>
   );
 }
