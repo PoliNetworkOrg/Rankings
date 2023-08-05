169de58ef131e32ae36c81a62647d02b51f27221
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2023-08-05 20:27:50 +0200

    feat: use badge if only 1 option in PhaseSelect (#204)
    * fix(LocationCombobox): show badge when 1 location such as LocationTabs
    
    * refactor: PhaseSelect
    
    * refactor(PhaseSelect): create Group and Ranking chunked components

---
+++ src/routes/viewer/PhaseSelect/GroupSelect/Tabs.tsx
@@ -0,0 +1,46 @@
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  PhaseGroup,
+  PhaseGroups,
+} from "@/utils/types/data/parsed/Index/RankingFile";
+
+export type GroupTabsProps = {
+  groups: PhaseGroups;
+  selectedGroup: PhaseGroup;
+  onChange: (group: PhaseGroup) => void;
+};
+
+export default function GroupTabs({
+  groups,
+  selectedGroup,
+  onChange,
+}: GroupTabsProps) {
+  function handleChange(value: string): void {
+    const group = groups.get(value);
+    if (!group) return;
+
+    onChange(group);
+  }
+
+  return (
+    selectedGroup && (
+      <Tabs
+        value={selectedGroup.value}
+        onValueChange={handleChange}
+        className="flex flex-1 overflow-x-hidden"
+      >
+        <TabsList className="flex overflow-x-hidden">
+          {groups.valuesArr().map((group) => (
+            <TabsTrigger
+              className="block"
+              value={group.value}
+              key={group.value}
+            >
+              {group.label}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+      </Tabs>
+    )
+  );
+}
