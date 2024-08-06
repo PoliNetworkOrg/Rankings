fb7b20e45f9a6e5a26bc9f4c9b32ed8f574dd323
userbot_github <actions@github.com>
2024-08-06 23:58:32 +0200

    Update root.tsx

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,6 @@
-import { ErrorComponent, RootRoute } from "@tanstack/router";
 import Layout from "@/components/Layout";
 import { RouterContext } from "@/router";
+import { ErrorComponent, RootRoute } from "@tanstack/react-router";
 
 export const rootRoute = RootRoute.withRouterContext<RouterContext>()({
   component: Layout,
