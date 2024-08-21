c6d2831f47a44154ab6e202cd100474a43c3355c
Lorenzo <lorenzo.corallo@outlook.it>
2024-08-21 18:23:10 +0200

    feat(FilterBtn): render static/preselected option if only one available

--- src/routes/viewer/Table/FilterBtn.tsx
+++ src/routes/viewer/Table/FilterBtn.tsx
@@ -19,6 +19,7 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
+import { Removable } from "@/components/custom-ui/Removable";
 
 export type FilterOption<T> = {
   originalValue: T;
@@ -41,7 +42,7 @@ export function FilterBtn<TData, TValue>({
   const facets = column.getFacetedUniqueValues();
   const selectedValues = new Set(column?.getFilterValue() as string[]);
 
-  return (
+  return facets.size >= 2 ? (
     <Popover>
       <PopoverTrigger asChild>
         <Button
@@ -156,5 +157,16 @@ export function FilterBtn<TData, TValue>({
         </Command>
       </PopoverContent>
     </Popover>
+  ) : (
+    <Removable showRemove={false}>
+      {title}
+      <Separator
+        orientation="vertical"
+        className="mx-2 h-4 bg-slate-400 dark:bg-slate-600"
+      />
+      {facets.size === 1 && Array.from(facets.keys())[0]
+        ? options[0].label
+        : "N/A"}
+    </Removable>
   );
 }
